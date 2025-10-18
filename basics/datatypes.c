52a046fe9850c654b122c7b3f841c5002b563e28
Rishav Kumar <digitalrishavofficial@gmail.com>
2025-10-18 14:04:20 +0530

    Day 8 - Data Types in C

---
+++ basics/datatypes.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+    int r = 7;
+    float v = 4 * 3.14 * r * r * r / 3;
+    printf("The Volume of Sphere is : %f", v);
+
+// Output - The Volume of Sphere is : 1436.026611
+    return 0;
+
+}
+
+
+
+
+/*
+
+--Basic Data Types in C--
+
+int 
+float
+double 
+char
+
+--Derived Datatypes in C--
+
+array 
+pointer
+structure 
+union
+function
+
+--Enumeration DataTypes in C--
+
+enum
+
+--Void Datatypes in C--
+
+void
+
+*/
